4b7568aad53ef8e135becf900d252535f7531130
lmosele <lucasmosele@gmail.com>
2015-03-02 14:26:08 -0500

    MORE pages
    adding portfolio case studies

--- js/classie.js
+++ js/classie.js
@@ -85,6 +85,6 @@ jQuery(document).ready(function(){
   jQuery('.skillbar').each(function(){
     jQuery(this).find('.skillbar-bar').animate({
       width:jQuery(this).attr('data-percent')
-    },6000);
+    },15000);
   });
 });
